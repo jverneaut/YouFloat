a397125df75e84d26ce2a4bb7416bdd37db4e31b
jverneaut <jverneaut@gmail.com>
2018-07-11 15:24:49 +0200

    Styling

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let playerOriginalHeight = 0;
 let autoplay = true;
 
-window.addEventListener("scroll", event => {
-  if (window.location.href.indexOf("watch") !== -1) {
+window.addEventListener('scroll', event => {
+  if (window.location.href.indexOf('watch') !== -1) {
     const player = document.querySelector('#player.style-scope');
     if (player.clientWidth === 0) {
       player = document.querySelector('#player-theater-container');
@@ -10,7 +10,7 @@ window.addEventListener("scroll", event => {
 
     const playerBoundingClientRect = player.getBoundingClientRect();
 
-    const autoplayBtn = document.getElementsByTagName("paper-toggle-button")[0];
+    const autoplayBtn = document.getElementsByTagName('paper-toggle-button')[0];
 
     let scrollTop = event.srcElement.scrollingElement.scrollTop
     if (scrollTop < 40) {
@@ -43,25 +43,23 @@ window.addEventListener("scroll", event => {
       }
     }
 
-
-
     const html5Player = document.getElementsByClassName('html5-main-video')[0];
-    html5Player.classList.toggle("video-float", isSmallPlayer);
+    html5Player.classList.toggle('video-float', isSmallPlayer);
 
-    const playerContainer = document.querySelector("#player-container-outer");
+    const playerContainer = document.querySelector('#player-container-outer');
     if (playerContainer) {
-      playerContainer.classList.toggle("container-float", isSmallPlayer);
+      playerContainer.classList.toggle('container-float', isSmallPlayer);
     }
 
-    const theaterPlayerContainer = document.querySelector("#player-theater-container");
+    const theaterPlayerContainer = document.querySelector('#player-theater-container');
     if (theaterPlayerContainer) {
-      theaterPlayerContainer.classList.toggle("container-float", isSmallPlayer);
+      theaterPlayerContainer.classList.toggle('container-float', isSmallPlayer);
     }
 
     const moviePlayer = document.querySelector('#movie_player')
     moviePlayer.classList.toggle('ytp-small-mode', isSmallPlayer);
 
-    const chrome_bottom = document.getElementsByClassName("ytp-chrome-bottom")[0];
-    chrome_bottom.classList.toggle("chrome-bottom-float", isSmallPlayer);
+    const chrome_bottom = document.getElementsByClassName('ytp-chrome-bottom')[0];
+    chrome_bottom.classList.toggle('chrome-bottom-float', isSmallPlayer);
   }
 });
